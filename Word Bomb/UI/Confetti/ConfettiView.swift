98172f0a292a227168241df73730093d1a81d91b
beetee17 <brandon.thio@yahoo.com.sg>
2021-12-27 00:27:42 +0800

    Adds back ConfettiView code that was wrongly deleted

---
+++ Word Bomb/UI/Confetti/ConfettiView.swift
@@ -0,0 +1,47 @@
+//
+//  ConfettiView.swift
+//  Word Bomb
+//
+//  Created by Brandon Thio on 25/12/21.
+//  From https://betterprogramming.pub/creating-confetti-particle-effects-using-swiftui-afda4240de6b
+
+import SwiftUI
+import simd
+
+struct ConfettiView: View {
+    var body: some View {
+        ZStack {
+            ForEach(1...200, id: \.self) { index in
+            Rectangle()
+                .modifier(Particle())
+                .offset(x: 0, y : 800)
+            }
+        }
+        .onAppear() {
+            Game.playSound(file: "confetti")
+        }
+    }
+}
+struct ConfettiView_Previews: PreviewProvider {
+    
+    struct ConfettiView_Harness: View {
+        
+        @State var animate = false
+        
+        var body: some View {
+            ZStack {
+                Button(animate ? "ANIMATION COMPLETE" : "ANIMATE") {
+                    animate.toggle()
+                }
+                if animate {
+                    ConfettiView()
+                }
+                
+            }
+        }
+    }
+    static var previews: some View {
+        ConfettiView_Harness()
+    }
+}
+
