ab52f7581b90e0d10351d5d2de9d670b1397c448
beetee17 <brandon.thio@yahoo.com.sg>
2021-12-27 00:37:26 +0800

    Remove commented out code

--- Word Bomb/UI/Gameplay/PlayerView.swift
+++ Word Bomb/UI/Gameplay/PlayerView.swift
@@ -35,58 +35,13 @@ struct PlayerView: View {
     }
 }
 
-
 struct PlayerView_Previews: PreviewProvider {
-    
+
     static var previews: some View {
-        PlayerView().environmentObject(WordBombGameViewModel())
+
+        Group {
+            PlayerView().environmentObject(WordBombGameViewModel.preview(numPlayers: 3))
+            PlayerView().environmentObject(WordBombGameViewModel.preview(numPlayers: 2))
+        }
     }
 }
-
-//struct PlayerView: View {
-//
-//    @EnvironmentObject var viewModel: WordBombGameViewModel
-//    @Binding var players: Players
-//    @State var trainingMode: Bool
-//
-//    var body: some View {
-//
-//        ZStack {
-//            switch viewModel.model.players.queue.count {
-//
-//            case 3...Int.max:
-//                PlayerCarouselView()
-//                    .transition(.scale)
-//            case 2:
-//                TwoPlayerView()
-//                    .transition(.scale)
-//            default:
-//                if viewModel.trainingMode {
-//                    HStack {
-//                        Text("Lives: ").boldText()
-//                        PlayerLives(player: $viewModel.model.players.current)
-//                    }
-//                    .offset(x: 0, y: Device.height*0.2)
-//
-//                } else {
-//                    MainPlayer(player: viewModel.model.players.current, animatePlayer: .constant(false))
-//                        .offset(x: 0, y: Device.height*0.1)
-//                        .transition(.scale)
-//                }
-//
-//            }
-//        }
-//        .animation(Game.mainAnimation)
-//    }
-//}
-//
-//struct PlayerView_Previews: PreviewProvider {
-//
-//    static var previews: some View {
-//
-//        Group {
-//            PlayerView().environmentObject(WordBombGameViewModel.preview(numPlayers: 3))
-//            PlayerView().environmentObject(WordBombGameViewModel.preview(numPlayers: 2))
-//        }
-//    }
-//}
