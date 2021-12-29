0ea5328de57d15ce3952cda56c343e3eddf6f902
beetee17 <brandon.thio@yahoo.com.sg>
2021-12-29 17:07:18 +0800

    Add function for backgrounding tasks with completion handler

--- Word Bomb/Utils/Extensions.swift
+++ Word Bomb/Utils/Extensions.swift
@@ -88,3 +88,18 @@ extension Collection where Index == Int {
         return self[self.index(self.endIndex, offsetBy: -backBy)]
     }
 }
+
+extension DispatchQueue {
+
+    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> Void)? = nil) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            background?()
+            if let completion = completion {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                    completion()
+                })
+            }
+        }
+    }
+
+}
