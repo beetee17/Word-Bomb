fe268af4ac9ed85e5e6a2b84797c4cc6fd593598
beetee17 <brandon.thio@yahoo.com.sg>
2021-12-30 22:02:58 +0800

    Include ScoreCounter view into TopBar

--- Word Bomb/UI/TopBar/TopBarView.swift
+++ Word Bomb/UI/TopBar/TopBarView.swift
@@ -11,44 +11,48 @@ import GameKit
 struct TopBarView: View {
     
     @EnvironmentObject var viewModel: WordBombGameViewModel
+    @Binding var gamePaused: Bool
     @Binding var showMatchProgress: Bool
     var gkMatch: GKMatch?
     
     var body: some View {
         
         HStack {
-            
-            switch gkMatch == nil {
-            case true:
-                PauseButton(viewToShow: $viewModel.viewToShow)
-            case false:
-                GKQuitButton()
-            }
-
             Spacer()
+                .overlay(
+                    ZStack {
+                        switch gkMatch == nil {
+                        case true:
+                            PauseButton(gamePaused: $gamePaused)
+                        case false:
+                            GKQuitButton()
+                        }
+                    }
+                )
+                .frame(width:Device.width*0.2)
             
             TimerView(
                 players: $viewModel.model.players,
-                timeLeft: $viewModel.model.timeKeeper.timeLeft,
-                timeLimit: viewModel.model.timeKeeper.timeLimit,
-                animateExplosion: $viewModel.model.media.animateExplosion,
-                playRunningOutOfTimeSound: $viewModel.model.media.playRunningOutOfTimeSound
+                timeLeft: $viewModel.model.controller.timeLeft,
+                timeLimit: viewModel.model.controller.timeLimit,
+                animateExplosion: $viewModel.model.controller.animateExplosion
             )
                 .offset(x: gkMatch == nil ? 0 : -20)
+                .frame(width:Device.width*0.5)
+                .offset(x:Device.width*0.025)
             
             Spacer()
-            
-            if .gameOver == viewModel.model.gameState {
-                RestartButton()
-            } else if !showMatchProgress {
-                Button("\(viewModel.model.numCorrect)") {
-                    showMatchProgress.toggle()
-                }
-                .foregroundColor(.white)
-            }
-            
+                .overlay(
+                    ZStack {
+                        if .gameOver == viewModel.model.gameState {
+                            RestartButton()
+                        } else if !showMatchProgress {
+                            ScoreCounter(score: $viewModel.model.numCorrect) {
+                                showMatchProgress.toggle()
+                            }
+                        }
+                    })
         }
-        .padding(.horizontal, 20)
         // The top bar is smaller when there are less than 3 players due to the bomb explosion animation.
         .frame(height: Game.miniBombSize*1.5)
         .offset(x: 0,y: -Device.height*0.03)
@@ -57,7 +61,25 @@ struct TopBarView: View {
 
 
 struct TopBarView_Previews: PreviewProvider {
+    
+    struct TopBarView_Harness: View {
+        @ObservedObject var viewModel = WordBombGameViewModel()
+        
+        var body: some View {
+            VStack {
+                TopBarView(gamePaused: .constant(false), showMatchProgress: .constant(false))
+                    .environmentObject(viewModel)
+                Game.MainButton(label: "1", systemImageName: "plus.circle") {
+                    viewModel.model.numCorrect += 1
+                }
+                Game.MainButton(label: "10", systemImageName: "multiply.circle") {
+                    viewModel.model.numCorrect *= 10
+                }
+            }
+            .background(Color("Background"))
+        }
+    }
     static var previews: some View {
-        TopBarView(showMatchProgress: .constant(false)).environmentObject(WordBombGameViewModel())
+        TopBarView_Harness()
     }
 }
