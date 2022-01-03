c46f2e17928ea68096006cb1b41c037afa537691
beetee17 <brandon.thio@yahoo.com.sg>
2022-01-03 14:08:58 +0800

    Tweak dynamic difficulty adjustment
    - based on some analysis using Python

--- Word Bomb/Models/ContainsWordGameModel.swift
+++ Word Bomb/Models/ContainsWordGameModel.swift
@@ -22,8 +22,6 @@ struct ContainsWordGameModel: WordGameModel {
     var pivot: Int
     var numTurns = 0
     var numTurnsBeforeDifficultyIncrease = 2
-
-    var syllableDifficulty = UserDefaults.standard.double(forKey: "Syllable Difficulty")
     
     init(variants: [String: [String]] = [:], queries: [(String, Int)], totalWords: Int) {
         self.words = variants.keys.sorted(by: {$0 < $1})
@@ -33,7 +31,7 @@ struct ContainsWordGameModel: WordGameModel {
         self.queries = queries.sorted() { $0.1 < $1.1 }
         self.queriesCopy = self.queries
 
-        self.pivot = queries.bisect(at: Int(syllableDifficulty*100.0))
+        self.pivot = 1000
     }
     
     mutating func process(_ input: String, _ query: String? = nil) -> Response {
@@ -45,7 +43,6 @@ struct ContainsWordGameModel: WordGameModel {
         numTurns += 1
         
         let searchResult = words.search(element: input)
-        //        return ("correct", getRandQuery(input))
         if usedWords.contains(input) {
             print("\(input.uppercased()) ALREADY USED")
             return Response(status: .Used)
@@ -93,6 +90,24 @@ struct ContainsWordGameModel: WordGameModel {
     }
     
     func getScore(for input: String, and query: String?) -> Int {
+        if let frequency = queriesCopy.first(where: { $0.0 == query })?.1 {
+            print("\(query) frequency \(frequency)")
+            switch frequency {
+            case 0...25:
+                return 25
+            case 26...100:
+                return 15
+            case 101...200:
+                return 10
+            case 201...500:
+                return 5
+            case 501...1000:
+                return 3
+            default:
+                return 1
+            }
+        }
+        print("Query \(query) not found?")
         return 1
     }
     
@@ -118,23 +133,15 @@ struct ContainsWordGameModel: WordGameModel {
         // TODO: Make difficulty smoother and customisable if possible
         // Need to graph this distribution over time to visualise what's happening
         for i in queries.indices {
-            switch i == pivot {
-            case true: break
-            case false:
-                let weight = queries[i].1
-                let originalOffset = Double(abs(queriesCopy[i].1 - queriesCopy[pivot].1))
-                let currentOffset = Double(abs(queries[i].1 - queries[pivot].1))
-                
-                if i < pivot {
-                    
-                    queries[i] = (queries[i].0, min(queriesCopy.last!.1, weight + Int(currentOffset*0.05) + Int(originalOffset*0.1)))
-                    
-                }
-                
-                else {
-                    queries[i] = (queries[i].0, max(0, weight - Int(currentOffset*0.05) - Int(originalOffset*0.1)))
-                }
+            let weight = queries[i].1
+            let currentOffset = Int(Double(abs(queries[i].1 - queries[pivot].1)) * 0.05)
+            
+            if i < pivot {
+                queries[i] = (queries[i].0, weight + currentOffset)
+            } else {
+                queries[i] = (queries[i].0, weight - currentOffset)
             }
+
             
             
             //            if i < 600 && i > 590{
