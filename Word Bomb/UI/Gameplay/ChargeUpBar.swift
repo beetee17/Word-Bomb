c693c74285c76dc660960595afbf9339c88de201
beetee17 <brandon.thio@yahoo.com.sg>
2022-01-04 11:56:56 +0800

    Add graphics to display current multiplier

--- Word Bomb/UI/Gameplay/ChargeUpBar.swift
+++ Word Bomb/UI/Gameplay/ChargeUpBar.swift
@@ -34,28 +34,27 @@ struct Shimmer: View {
 }
 
 struct ChargeUpBar: View {
+    
+    @ObservedObject var imagePicker: StarImagePicker
     var value: Int
+    var multiplier: Int
     var invert: Bool
     
     var body: some View {
         
-        HStack {
-            //            Image(systemName: "star.fill")
-            //                .resizable()
-            //                .frame(width:40, height:40)
-            //                .scaledToFit()
-            //                .offset(x: 20)
-            //                .zIndex(1)
-            //                .foregroundColor(.yellow)
-           
+        VStack {
+
+            let val = Float(value)/Float(Game.getMaxCharge(for: multiplier))
             
             GeometryReader { geometry in
                 
+                                             
                 let width = invert
                 ? geometry.size.width
-                : min(CGFloat(Float(value)/50) * geometry.size.width, geometry.size.width)
+                : min(CGFloat(val) * geometry.size.width, geometry.size.width)
+                
                 let height = invert
-                ? min(CGFloat(Float(value)/50)*geometry.size.height, geometry.size.height)
+                ? min(CGFloat(val) * geometry.size.height, geometry.size.height)
                 : geometry.size.height
                 
                 let widthBinding = Binding(
@@ -84,10 +83,28 @@ struct ChargeUpBar: View {
                     RoundedRectangle(cornerRadius: 45)
                         .stroke(lineWidth: 5)
                         .foregroundColor(.black)
+
                     
                 }
                 .cornerRadius(45)
                 
+                if multiplier > 1 {
+                    Image(multiplier == 2 ? "x2" : "x3")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: invert ? geometry.size.width*2.5 : geometry.size.height)
+                        .pulseEffect()
+                        .rotationEffect(Angle(degrees: 15))
+                        .shadow(radius: 5)
+                        .if(invert) { $0.offset(x: -8,
+                                                y: geometry.size.height*0.9) }
+                        .if(!invert) { $0.offset(x: -5, y: 5) }  
+                }
+            }
+            .onChange(of: multiplier) { [multiplier] newValue in
+                if newValue > multiplier {
+                    imagePicker.getImage(for: .Combo)
+                }
             }
         }
     }
@@ -96,18 +113,29 @@ struct ChargeUpBar: View {
 struct ChargeUpBar_Previews: PreviewProvider {
     struct ChargeUpBar_Harness: View {
         @State private var value = 5
+        @State private var multiplier = 1
         var body: some View {
             ZStack {
                 Color("Background")
                 VStack {
-                    ChargeUpBar(value: value, invert: true)
-                        .frame(width: 30, height: 300)
+                    ChargeUpBar(
+                        imagePicker: StarImagePicker(),
+                        value: value,
+                        multiplier: multiplier,
+                        invert: true)
+                        .frame(width: 10, height: 100)
                                         
                     Game.MainButton(label: "CHARGE") {
                         value += 5
                     }
+                    Game.MainButton(label: "MULTIPLY") {
+                        withAnimation(.easeInOut) {
+                        multiplier += 1
+                        }
+                    }
                     Game.MainButton(label: "RESET") {
                         value = 0
+                        multiplier = 1
                     }
                     
                 }
