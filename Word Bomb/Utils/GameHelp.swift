bb4a938ff0280c7bccd603157b7dbc889783157b
beetee17 <brandon.thio@yahoo.com.sg>
2022-01-05 22:36:05 +0800

    Update help sheet information

--- Word Bomb/Utils/GameHelp.swift
+++ Word Bomb/Utils/GameHelp.swift
@@ -74,24 +74,39 @@ struct HelpSheet: ViewModifier {
 
 
 extension Game {
-    static let gameOverview = "Word Bomb is a multiplayer word game that allows you to test your quick-thinking and general knowledge of vocabulary or other custom categories such as countries.\n\nPlayers are tasked to form a word according to the game mode's instruction before their time runs out, which loses them a life. The last player standing is declared the winner of the game!"
+    static let gameOverview = "Word Bomb is a multiplayer word game that allows you to test your quick-thinking and general knowledge of vocabulary or other custom categories such as countries.\n\nPlayers gain points by forming a word according to the game mode's instruction before their time runs out, which loses them a life. The game ends when all players run out of lives. The player with the most points is declared the winner of the game!"
+    
+    static let gameFAQ1 = "Q: What happens if multiple players end with the same points? \n\nA: The tied players are given 1 life each to compete until a single winner is found!\n"
+    
+    static let gameFAQ2 = "\nQ: What is that yellow progress bar for? \n\nA: The progress bar beside each player's avatar measures the number of points gained without since he/she last lost a life. Fill it up to multiply your score!\n"
+    
+    static let gameFAQ3 = "\nQ: How does the points system work? \n\nA: In the Classic Words game, the points awarded for a correct answer is in proportion to how uncommon the syllable given is in the English Dictionary. \n\nIn Exact game modes, a constant number of points is awarded for each correct answer"
     
     static let classicHelp = "In a Classic game, players are given a randomly generated syllable, and your task is to come up with a valid word that contains the syllable."
     static let exactHelp = "In an Exact game, you must come up with an answer that is found in the mode's database. For example, a database of countries would mean players are only allowed to name countries. You can create your own custom database to play with friends!"
     static let reverseHelp = "A Reverse game is similar to the Exact game, with the added constraint that the answer must start with the ending letter of the previous player's answer."
     
-    static let startGameHelp = "Press to start a game! If you are not in an ongoing multiplayer game, you may start an offline, pass-and-play style game with a specified number of players and custom player names (you can change this in the settings menu). Useful if you do not have a good network connection.\n\nIf you are hosting other players via local multiplayer, start a game in the same way to play with them!"
-    static let gameCenterHelp = "In order to play a game using Game Center, you need to be friends with the person who you are trying to play with. To do this, proceed to Settings App ⭢ Game Center ⭢ Add Friends.\n\nAfter you are friends with the people you want to play with, you can play with them by simply navigating to Game Center ⭢ Host Game and inviting them to a game!"
+    static let singlePlayerHelp = "Play for a high score and compare them with your friends in the Single Player mode! Settings are fixed in single player games."
+    static let singlePlayerFAQ = "Q: Are there any differences in Single Player games? \n\nA: Apart from the preset settings, players are given a bonus when they use all 26 letters of the alphabet. Tap the icon at the top right corner to see which letters are still required!"
     
-    static let localNetworkHelp = "You can also play a multiplayer game with nearby players using the Local Network button. This works for players in close proximity and who do not want to use Game Center. You may also prefer to play via local network over game center as it supports custom modes!\n\nThe steps are as follows:\n\n1. Navigate to Multiplayer ⭢ Local Network ⭢ Host Game.\n\n2. You should see your friends' names pop up there!\n\n3. When the indicator beside a friend's name turns green, you may connect to each other by tapping on their names."
+    static let startGameHelp = "Press to start a game! Play solo, with Game Center friends, or even an offline, pass-and-play style game with a specified number of players and custom player names (this can be changed in the settings menu)."
+    
+    static let gameCenterHelp = "In order to play a game using Game Center, you need to be friends with the person who you are trying to play with. To do this, proceed to Settings App ⭢ Game Center ⭢ Add Friends.\n\nAfter you are friends with the people you want to play with, you can play with them by choosing this option!"
     
     static let createModeHelp = "The Create Mode button presents a sheet where you can create your own custom modes to play with friends."
+    
+    static let databaseHelp = "Here, you can search through the words used in the game to study and improve your future performances!\n\n You can also create your own database that can then be used in a custom mode to play with friends!"
+    
+    static let databaseHelpFAQ = "Q: Can I edit the inbuilt databases? \n\nA: It is not allowed to edit the default databases. However, if you would like to leverage them in your own database, simply make a copy of it and edit from there! \n\nYou may find this functionality in the 'Copy Existing' section in the 'Add Database' form or by force-pressing the name of the database you would like to duplicate."
     
     static let settingHelp = "Customise various settings of the game mechanics here. Relevant settings will also apply to online gameplay if you are the host!"
     
     static let helpMessages =
-        [HelpMessage(content: "Game Objective",
-                     subMessages: [HelpMessage(content: gameOverview)]),
+         [HelpMessage(content: "Game Objective",
+                     subMessages: [
+                        HelpMessage(content: gameOverview),
+                        HelpMessage(content: "FAQ", subMessages: [HelpMessage(content: gameFAQ1 + gameFAQ2 + gameFAQ3)])
+                    ]),
          
          HelpMessage(content: "Game Types",
                      subMessages: [
@@ -101,14 +116,14 @@ extension Game {
                         HelpMessage(content: "Reverse", subMessages: [HelpMessage(content: reverseHelp)])
                      ]),
          
-         HelpMessage(content: "Start Game", subMessages: [HelpMessage(content: startGameHelp)]),
-         
-         HelpMessage(content: "Multiplayer",
+         HelpMessage(content: "Single Player",
                      subMessages: [
-                        HelpMessage(content: "There are two ways to play an online game with your friends!"),
-                        HelpMessage(content: "Game Center", subMessages: [HelpMessage(content: gameCenterHelp)]),
-                        HelpMessage(content: "Local Network", subMessages: [HelpMessage(content: localNetworkHelp)])
-                     ]),
+                        HelpMessage(content: singlePlayerHelp),
+                        HelpMessage(content: "FAQ", subMessages: [HelpMessage(content: singlePlayerFAQ)])
+                    ]),
+         
+         HelpMessage(content: "Online Multiplayer",
+                     subMessages: [HelpMessage(content: gameCenterHelp)]),
          
          HelpMessage(content: "Create Mode", subMessages: [HelpMessage(content: createModeHelp)]),
          
