4d5cba8a0f7eff58c74e881aeea313709754f4b2
beetee17 <brandon.thio@yahoo.com.sg>
2022-01-05 20:42:49 +0800

    Add reward option for training mode - appears when player uses all letters in the alphabet - can select from two rewards: +1 life or +5s

--- Word Bomb/UI/TopBar/CorrectCounter.swift
+++ Word Bomb/UI/TopBar/CorrectCounter.swift
@@ -16,6 +16,7 @@ struct CorrectCounter: View {
         
         Button(action: {
             AudioPlayer.playSound(.Select)
+            action()
         }) {
             HStack {
                 
@@ -46,29 +47,25 @@ struct CorrectCounter: View {
                     }
                 }
             }
-            
-            
-            
         }
         .buttonStyle(ScaleEffect())
-        
     }
 }
 
 struct RewardOptions: View {
-    @State var isShowing: Bool
-    var addLiveAction: () -> Void
+    var isShowing: Bool
+    var addLifeAction: () -> Void
     var addTimeAction: () -> Void
     
     var body: some View {
-        VStack(spacing: 20) {
-            if isShowing {
-                Button(action: addTimeAction) {
+        if isShowing {
+            VStack(spacing: 20) {
+                
+                Button(action: addLifeAction) {
                     Image(systemName: "heart.fill")
                         .resizable().scaledToFit()
                         .frame(width: 30, height: 30)
                         .foregroundColor(.red)
-                        .transition(.offset(y:-45).combined(with: .opacity))
                         .shadow(color: .green.opacity(0.5), radius: 3)
                         .pulseEffect()
                 }
@@ -79,35 +76,42 @@ struct RewardOptions: View {
                     Image(systemName: "stopwatch")
                         .resizable().scaledToFit()
                         .frame(width: 30, height: 30)
-                        .transition(.offset(y: -75).combined(with: .opacity))
                         .shadow(color: .green.opacity(0.7), radius: 3)
                         .pulseEffect()
+                        
                 }
                 .buttonStyle(ScaleEffect())
                 .onTapGesture { AudioPlayer.playSound(.Combo) }
                 
             }
+            .background(Color.black.opacity(0.3).blur(radius: 10))
+            .offset(y: 75)
+            .transition(.offset(y: -75).combined(with: .opacity))
         }
-        .background(Color.black.opacity(0.3).blur(radius: 10))
-        .offset(y: 75)
     }
 }
 struct CorrectCounter_Previews: PreviewProvider {
     struct CorrectCounter_Harness: View {
         @State var numCorrect = 0
-        
+        @State var showReward = false
         var body: some View {
             ZStack {
                 Color("Background")
                     .ignoresSafeArea()
                 VStack {
-                    CorrectCounter(numCorrect: numCorrect, action: {})
-                    Game.MainButton(label: "Plus One") {
-                        numCorrect += 1
+                    
+                    ZStack {
+                        CorrectCounter(numCorrect: numCorrect, action: { withAnimation(.easeInOut) { showReward.toggle() } })
+                        RewardOptions(isShowing: showReward, addLifeAction: {}, addTimeAction: {})
+                        
                     }
-                    Game.MainButton(label: "Times Ten") {
-                        numCorrect *= 10
-                    }
+                    
+                    //                    Game.MainButton(label: "Plus One") {
+                    //                        numCorrect += 1
+                    //                    }
+                    //                    Game.MainButton(label: "Times Ten") {
+                    //                        numCorrect *= 10
+                    //                    }
                 }
             }
         }
