54f9d7dd9a8ffa3a3917c265d72f9dbb4c539aa5
beetee17 <brandon.thio@yahoo.com.sg>
2022-01-09 12:37:14 +0800

    Lower volume of sounds and support for UserDefaults to adjust volume to user preference

--- Word Bomb/Utils/AudioPlayer.swift
+++ Word Bomb/Utils/AudioPlayer.swift
@@ -25,29 +25,29 @@ enum Sound {
     var info: (filename: String, volume: Float) {
         switch self {
         case .Explosion:
-            return ("explosion", 0.2)
+            return ("explosion", 0.05)
         case .RunningOutOfTime:
-            return ("hissing", 0.1)
+            return ("hissing", 0.025)
         case .Select:
-            return ("pop", 0.2)
+            return ("pop", 0.05)
         case .Cancel:
-            return ("back", 0.2)
+            return ("back", 0.05)
         case .Banner:
-            return ("ping", 0.6)
+            return ("ping", 0.05)
         case .Alert:
-            return ("alert", 0.5)
+            return ("alert", 0.05)
         case .Correct:
-            return ("correct", 0.4)
+            return ("correct", 0.05)
         case .Wrong:
-            return ("wrong-2", 0.4)
+            return ("wrong-2", 0.05)
         case .Combo:
-            return ("combo", 0.4)
+            return ("combo", 0.05)
         case .Victory:
-            return ("victory", 0.4)
+            return ("victory", 0.05)
         case .BGMusic:
-            return ("Bicycle", 0.5)
+            return ("Bicycle", 0.1)
         case .GamePlayMusic:
-            return ("Monkeys-Spinning-Monkeys", 0.5)
+            return ("Monkeys-Spinning-Monkeys", 0.1)
         }
     }
 }
@@ -56,7 +56,7 @@ class AudioPlayer {
     
     private var player = AVAudioPlayer()
     
-    var volume: Float
+    var volume: Float = 0.01
     
     /// Useful to control the playback of ROOT sound when `timeLeft` is low. The sound can only be played when `isReady` is true.
     var isReady = true
@@ -65,28 +65,40 @@ class AudioPlayer {
     var duration: Double { player.duration }
     
     init(sound: Sound, type: String = "wav", numLoops: Int = 0) {
-        self.volume = sound.info.volume
         
         if let path = Bundle.main.path(forResource: sound.info.filename, ofType: type) {
             do {
+                _ = try? AVAudioSession
+                            .sharedInstance()
+                            .setCategory(AVAudioSession.Category.ambient,
+                                         mode: .default,
+                                         options: .mixWithOthers)
                 player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
                 // Negative value for infinite loop
                 player.numberOfLoops = numLoops
                 // wait for previous sound track to fade out
                 player.prepareToPlay()
-                player.setVolume(volume, fadeDuration: 0)
+                player.setVolume(self.volume, fadeDuration: 0)
                 
             } catch {
                 print("Error initing AudioPlayer: \(error.localizedDescription)")
             }
         }
     }
+    func setVolume(to newVolume: Float = 0.5) {
+        self.volume = newVolume
+    }
+    
+    func scaleVolume(by scale: Float = 1) {
+        player.setVolume(self.volume*scale, fadeDuration: 0)
+    }
     
     func play(with fadeDuration: Double = 0) {
         player.setVolume(0, fadeDuration: TimeInterval(fadeDuration))
         player.play()
-        player.setVolume(volume, fadeDuration: TimeInterval(fadeDuration))
+        player.setVolume(self.volume, fadeDuration: TimeInterval(fadeDuration))
     }
+    
     func pause(with fadeDuration: Double = 0, reset: Bool = false) {
         player.setVolume(0, fadeDuration: TimeInterval(fadeDuration))
         DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + fadeDuration) {
@@ -110,10 +122,13 @@ extension AudioPlayer {
     static var shared: AudioPlayer? = nil
     
     static func playSound(_ sound: Sound, type: String = "wav") {
+        guard UserDefaults.standard.bool(forKey: "Sound FXs") else { return }
+        
         AudioPlayer.shared?.pause()
         AudioPlayer.shared = nil
         
         AudioPlayer.shared = AudioPlayer(sound: sound, type: type)
+        AudioPlayer.shared?.setVolume(to: sound.info.volume*UserDefaults.standard.float(forKey: "SoundFX Volume"))
         AudioPlayer.shared?.play()
         
         if AudioPlayer.root.isPlaying {
@@ -135,9 +150,11 @@ extension AudioPlayer {
     static var soundTrack: AudioPlayer? = nil
     
     static func playSoundTrack(_ sound: Sound, delay: Double = 1) {
+        guard UserDefaults.standard.bool(forKey: "Soundtrack") else { return }
         AudioPlayer.soundTrack?.pause(with: delay)
         DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + delay) {
             AudioPlayer.soundTrack = AudioPlayer(sound: sound, type: "mp3", numLoops: -1)
+            AudioPlayer.soundTrack?.setVolume(to: sound.info.volume*UserDefaults.standard.float(forKey: "Soundtrack Volume"))
             AudioPlayer.soundTrack?.play(with: delay)
         }
     }
@@ -146,7 +163,11 @@ extension AudioPlayer {
     
     static func playROOTSound() {
         // This function is continuously called when `timeleft < 3`. We do not want the sound to keep replaying.
+        guard UserDefaults.standard.bool(forKey: "Sound FXs") else { return }
+        
         guard !AudioPlayer.root.isPlaying && AudioPlayer.root.isReady else { return }
+        AudioPlayer.root.player.numberOfLoops = 1
+        AudioPlayer.root.setVolume(to: Sound.RunningOutOfTime.info.volume*UserDefaults.standard.float(forKey: "SoundFX Volume"))
         AudioPlayer.root.play()
         AudioPlayer.root.isReady = false
     }
